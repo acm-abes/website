8e7ee4dcc824c885cf74a03c2c0b0f4407ad4be4
halomanlodestar <kunalranarj2005@gmail.com>
2024-05-25 22:11:36 +0530

    Fixed Event sitemaps

--- app/events/sitemap.ts
+++ app/events/sitemap.ts
@@ -2,11 +2,6 @@ import { MetadataRoute } from "next";
 import { routes } from "@/public/data/constants";
 import { getAllEvents } from "@/lib/utils";
 
-export async function generateSitemaps() {
-  const events = await getAllEvents();
-  return events.map((event) => ({ id: event.id }));
-}
-
 const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
   const { base } = routes;
 
