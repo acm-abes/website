29485220a59be955c0a07b400e89eecb3027042e
Kunal Rana <kunalranarj2005@gmail.com>
2025-12-06 21:16:42 +0530

    upgraded to nextjs 16

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,6 @@
 /** @format */
 
-import { PrismaClient } from "../../generated/prisma/client";
+import { PrismaClient } from "../../generated/prisma";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
